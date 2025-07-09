116adfdf3997bb388a7c034583a6f09ff72798de
Shruti Myatra <shrutimyatra101@gmail.com>
2025-07-09 13:15:03 +0530

    Initial Commit

---
+++ Myntra/MyntraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyntraApp.swift
+//  Myntra
+//
+//  Created by students on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MyntraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Myntra/ViewPages/Signup.swift
@@ -0,0 +1,26 @@
+//
+//  Signup.swift
+//  Myntra
+//
+//  Created by students on 08/07/25.
+//
+
+import SwiftUI
+
+struct Signup: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                HStack{
+                    SearchTextField(searchText: .constant(""), placeholder: "Search ")
+                        .padding(.horizontal, 20)
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    Signup()
+}
